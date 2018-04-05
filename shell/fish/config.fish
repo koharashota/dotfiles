4f74f4798b74a85b3d665d99d3f1d859208d7f99
koharashota <transparent.blue69@gmail.com>
2018-04-05 09:37:09 +0900

    :bug: fish configの重複箇所修正

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,9 +1,6 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
-end
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-  bind \c] peco_select_ghq_repository  # 追加
+  bind \c] peco_select_ghq_repository
 end
 status --is-interactive; and source (rbenv init -|psub)
 alias ll='ls -la'
