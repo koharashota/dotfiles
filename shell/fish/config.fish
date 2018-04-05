a69ac06326d614497f417c311f66b476d72adea8
小原　正大 <01010327@CA3548.local>
2018-04-05 09:09:56 +0900

    :+1: ghqの設定を追加

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,6 +1,10 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \c] peco_select_ghq_repository  # 追加
+end
 status --is-interactive; and source (rbenv init -|psub)
 alias ll='ls -la'
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
